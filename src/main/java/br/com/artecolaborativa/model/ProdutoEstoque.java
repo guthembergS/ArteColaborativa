056a97384f3cdcb4f97e65640fec3cc5f0a06607
guthembergS <gutheaugusto123@gmail.com>
2019-08-12 20:04:45 -0300

    criacao e mapeamento do estoque e inicio de implementacao da venda

---
+++ src/main/java/br/com/artecolaborativa/model/ProdutoEstoque.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.artecolaborativa.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author guto
+ */
+@Entity
+@Table(name = "PRODUTO_ESTOQUE")
+public class ProdutoEstoque implements Serializable{
+    @Id
+    @Column(name = "ID_PRODUTO_ESTOQUE")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long idProdutoEstoque;
+    
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ID_PRODUTO", referencedColumnName = "ID_PRODUTO",nullable = false )
+    protected Produto produto;
+    
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ID_LOJISTA", referencedColumnName = "ID_USUARIO",nullable = false )
+    protected Lojista lojista;
+    
+    @Column()
+    protected Long quantidade;
+    
+    public void setProduto(Produto produto){
+        this.produto = produto;
+    }
+    
+    public Produto getProduto() {
+        return produto;
+    }
+    
+    public void setLojista(Lojista logista){
+        this.lojista = lojista;
+    }
+    
+    public Lojista getLojista() {
+        return lojista;
+    }
+    
+    public void setQuantidade(Long quantidade){
+        this.quantidade = quantidade;
+    }
+    
+    public Long getQuantidade() {
+        return quantidade;
+    }
+    
+    public Long getIdProdutoEstoque(){
+        return idProdutoEstoque;
+    }
+    
+}
---
+++ src/main/java/br/com/artecolaborativa/model/VendaLojistaProduto.java
@@ -0,0 +1,57 @@
+package br.com.artecolaborativa.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author guto
+ */
+@Entity
+@Table(name = "VENDA_LOJISTA_PRODUTO")
+public class VendaLojistaProduto implements Serializable{
+    
+    @Id
+    @Column(name = "ID_VENDA_LOJISTA")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long idVendaLojista;
+    
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ID_ESTOQUE", referencedColumnName = "ID_PRODUTO_ESTOQUE",nullable = false )
+    protected ProdutoEstoque produtoEstoque;
+    
+    @Column(name="DATA_VENDA")
+    protected Date dataVenda;
+    
+    @Column(name="QUANTIDADE")
+    protected Integer quantidade;
+    
+    public Long getIdVenda(){
+        return idVendaLojista;
+    }
+    
+    public Date getDataVenda(){
+        return dataVenda;
+    }
+    
+    public void setDataVenda(Date dataVenda){
+        this.dataVenda = dataVenda;
+    }
+    
+    public ProdutoEstoque getProdutoEstoque(){
+        return produtoEstoque;
+    }
+    
+    public void setProdutoEstoque(ProdutoEstoque produtoEstoque){
+        this.produtoEstoque = produtoEstoque;
+    }
+}
