13b2592d000476154de1205250f4416e87830e62
guthembergS <gutheaugusto123@gmail.com>
2019-08-21 16:39:35 -0300

    Criação da classe dbunity

---
+++ src/test/java/br/com/artecolaborativa/test/DbUnitUtil.java
@@ -0,0 +1,53 @@
+package br.com.artecolaborativa.test;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.mysql.MySqlDataTypeFactory;
+import org.dbunit.ext.mysql.MySqlMetadataHandler;
+import org.dbunit.operation.DatabaseOperation;
+
+public class DbUnitUtil {
+
+    private static final String XML_FILE = "/dbunit/dataset.xml";
+
+    @SuppressWarnings("UseSpecificCatch")
+    public static void inserirDados() {
+        Connection conn = null;
+        IDatabaseConnection db_conn = null;
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/arte_colaborativa", "root", "root");
+            db_conn = new DatabaseConnection(conn, "arte_colaborativa");
+            DatabaseConfig dbConfig = db_conn.getConfig();
+            dbConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory());
+            dbConfig.setProperty(DatabaseConfig.PROPERTY_METADATA_HANDLER, new MySqlMetadataHandler());
+            FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+            builder.setColumnSensing(true);
+            //InputStream in = DbUnitUtil.class.getResourceAsStream(XML_FILE);
+            final URL url = DbUnitUtil.class.getResource(XML_FILE);
+            IDataSet dataSet = builder.build(url);
+            DatabaseOperation.CLEAN_INSERT.execute(db_conn, dataSet);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage(), ex);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+
+                if (db_conn != null) {
+                    db_conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex.getMessage(), ex);
+            }
+        }
+    }
+}
