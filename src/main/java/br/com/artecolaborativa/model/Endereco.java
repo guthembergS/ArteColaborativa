1ffcc154c7e2f1a9175afada3404edb2e626294e
taoalu <taoalu@gmail.com>
2019-09-04 13:30:26 -0300

    Criação dos testes para o endereço

--- src/main/java/br/com/artecolaborativa/model/Endereco.java
+++ src/main/java/br/com/artecolaborativa/model/Endereco.java
@@ -6,12 +6,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  *
  * @author guthemberg.souza
  */
+
 @Entity
+@NamedQueries(
+        {
+            @NamedQuery(
+                    name = "Endereco.PorId",
+                    query = "SELECT e FROM Endereco e WHERE e.idEndereco = :id"
+            )
+        }
+)
+
 public class Endereco implements Serializable{
     @Id
     @Column(name = "ID_ENDERECO")
