a87caaa2e8b6718f0a6a6afbe8008ff7f0b41bf6
guthembergS <gutheaugusto123@gmail.com>
2019-09-26 19:14:17 -0300

    insercao de validations

---
+++ src/main/java/br/com/artecolaborativa/model/ValidaVendaProduto.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.artecolaborativa.model;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+/**
+ *
+ * @author guto
+ */
+@Target( {ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ValidadorVendaProduto.class)
+@Documented
+public @interface ValidaVendaProduto {
+    String message() default "{exemplo.jpa.Endereco.estado}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+    
+}
